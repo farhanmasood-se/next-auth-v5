8ad5ee7f280d4c8d44ba9f19821b83e76eda0f75
Farhan Masood <84273944+farhanmasood-se@users.noreply.github.com>
2024-08-11 15:10:57 +0500

    register page

---
+++ app/auth/register/page.tsx
@@ -0,0 +1,12 @@
+import { Login } from '@/actions/login';
+import { RegisterForm } from '@/components/auth/register-form';
+
+const Register = () => {
+  return (
+    <div>
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default Login;
--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -4,7 +4,7 @@ import * as z from 'zod';
 import { useForm } from 'react-hook-form';
 import { CardWrapper } from './card-wrapper';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { LoginSchema } from '@/schemas';
+import { RegisterSchema } from '@/schemas';
 import {
   Form,
   FormControl,
@@ -17,25 +17,26 @@ import { Input } from '../ui/input';
 import { Button } from '../ui/button';
 import { FormError } from '../form-error';
 import { FormSuccess } from '../form-success';
-import { Login } from '@/actions/login';
 import { useState, useTransition } from 'react';
+import { Register } from '@/actions/register';
 
-export const LoginForm = () => {
+export const RegisterForm = () => {
   const [error, setError] = useState<string | undefined>('');
   const [success, setSuccess] = useState<string | undefined>('');
   const [isPending, startTrasition] = useTransition();
 
-  const form = useForm<z.infer<typeof LoginSchema>>({
-    resolver: zodResolver(LoginSchema),
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
     defaultValues: {
       email: '',
       password: '',
+      name: '',
     },
   });
 
-  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+  const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
     startTrasition(() => {
-      Login(values).then((data) => {
+      Register(values).then((data) => {
         setError(data?.error);
         setSuccess(data?.success);
       });
@@ -44,14 +45,33 @@ export const LoginForm = () => {
 
   return (
     <CardWrapper
-      headerLabel="Welcome Back"
-      backButtonLabel="Don't have an account?"
-      backButtonHref="/auth/register"
+      headerLabel="Create an Account"
+      backButtonLabel="Already have an account?"
+      backButtonHref="/auth/login"
       showSocial={true}
     >
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
           <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="John Doe"
+                      type="name"
+                      disabled={isPending}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
             <FormField
               control={form.control}
               name="email"
